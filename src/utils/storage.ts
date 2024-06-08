4314d6a28a57c886eebd1fa95d1634302c57cfe9
dekguh <kdkteguhmahesa@gmail.com>
2024-06-08 10:09:56 +0800

    chore: remove unused files

--- src/utils/storage.ts
+++
@@ -1,3 +0,0 @@
-export const getStorageJwt = () : string => localStorage.getItem('token') ?? ''
-export const setStorageJwt = (token : string) => localStorage.setItem('token', token)
-export const clearStorageJwt = () => localStorage.removeItem('token')
